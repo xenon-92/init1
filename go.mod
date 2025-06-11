a5352a6e77bd02bec9a12ebd02416bd92f25fbf9
Sumit Jana <sumit.jana@hyland.com>
2025-06-11 11:10:26 +0530

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module init1_container
+
+go 1.21.4
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	fmt.Println("from init container")
+	log.Println("from init container.........")
+}
